3cd4f6b28aaf082fe2de509eb62271e727f9b490
Mikhail Vyachin <mikhailvyachin2004@gmail.com>
2024-03-21 15:43:56 +0300

    c

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import _ from "lodash";
 console.log(_.last(['one','two']));
 console.log('hello');
+const b = (a, b) => {
+   const b = Math.random()
+  const  a = Math.random()
+  const  c = b + a
+  return c
+    
+}

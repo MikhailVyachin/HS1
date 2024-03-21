3e94a127574a4df3ff5ce9a927eb04864200b128
Mikhail Vyachin <mikhailvyachin2004@gmail.com>
2024-03-21 16:52:02 +0300

    c

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import _ from "lodash";
-console.log(_.last(['one','two']));
-console.log('hello');
-const b = (a, b) => {
-   const b = Math.random()
-  const  a = Math.random()
-  const  c = b + a
-  return c
-    
-}
+
+const user = 'black canary'
+console.log(user)
+export default user
